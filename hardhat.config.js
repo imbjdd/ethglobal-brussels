92190e232d055036d4464de635cdbb0b830d1411
Riki0923 <83667276+Riki0923@users.noreply.github.com>
2024-07-12 21:21:14 +0200

    Krisztian push - dummy smart contract

---
+++ hardhat.config.js
@@ -0,0 +1,30 @@
+require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
+
+
+module.exports = {
+  solidity: "0.8.24",
+  networks: {
+    //Add extra chains as needed 
+    hardhat: {
+      chainId: 11155111,
+    },
+    sepolia: {
+      url: `${process.env.ALCHEMY_SEPOLIA_URL}`,
+      accounts: [`0x${process.env.PRIVATE_KEY_METAMASK0}`
+      ],
+    },
+    arbitrumSepolia: {
+      url: `${process.env.ALCHEMY_ARBITRUM_SEPOLIA_URL}`,
+      accounts: [`0x${process.env.PRIVATE_KEY_METAMASK0}`
+        ],
+    }
+  },
+  defaultNetwork: "hardhat",
+  etherscan: {
+    apiKey: "RVQ9MVJYI13NNK3SYF1412K2JHHVX2CJ88"
+  },
+  // sourcify: {
+  //   enabled: true
+  // }
+};
