e67f74b668ad91eaa18771fe3382f7fd1fed904f
imbjdd <b0bb1e@protonmail.com>
2024-07-13 03:43:16 +0200

    fix: deploy on vercel

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
+// next.config.js
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  webpack: (config) => {
+    config.externals.push('pino-pretty', /* add any other modules that might be causing the error */);
+    return config;
+  },
+};
 
 export default nextConfig;
