ace92c5ce68caef1212f9c3bc49767dc9fff0ce2
Riki0923 <83667276+Riki0923@users.noreply.github.com>
2024-07-13 17:19:39 +0200

    Krisztian - TheGraph update

--- src/app/page.js
+++ src/app/page.js
@@ -13,8 +13,12 @@ import {
   NavigationMenuViewport,
 } from "@/components/ui/navigation-menu"
 
+import ExampleComponent from "../components/ui/graph";
 
 export default function Home() {
+
+  ExampleComponent()
+  
   const datasets = [{name:"Airport passengers - 2024", description: "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, co", price: "180"}, {name:"Airport passengers - 2024", description: "This dataset comes from Europa", price: "180"},
     {name:"Airport passengers - 2024", description: "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, co", price: "180"}, {name:"Airport passengers - 2024", description: "This dataset comes from Europa", price: "180"}]
   return (
